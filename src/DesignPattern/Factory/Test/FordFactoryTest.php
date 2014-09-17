2c2cd217ef465d4b12840b4b89d43064355782fa
JanHuang <384099566@qq.com>
2014-09-17 19:19:09 +0800

    update phpunit

---
+++ src/DesignPattern/Factory/Test/FordFactoryTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * ClassName: FordFactoryTest
+ * User: JAN
+ * Date: 14-9-17
+ * Time: 下午7:10
+ * Link: http://blog.segmentfault.com/janhuang
+ */
+
+namespace DesignPattern\Factory\Test;
+
+class FordFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testOne()
+    {
+        return '1';
+    }
+}
+ 
